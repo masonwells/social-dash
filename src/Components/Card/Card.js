cfedb10ae4a6e0917f80f195e3a95f2249fa90e4
masonwells <masonwbrockbank@gmail.com>
2018-08-02 20:57:52 -0600

    fixing bugs

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,29 +2,31 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 export default class Card extends Component {
-        constructor(){
-          super()
-          this.state = {
-            card: []
-          }
-        }
+  constructor() {
+    super()
+    this.state = {
+      card: []
+    }
+  }
 
 
-componentDidMount(){
-  const {social} = this.props
-  console.log(social)
-  axios.get(`/social/goals/${social}`).then( res => {
-    this.setState({card: res.data})}
-  )
-}
+  componentDidMount() {
+    const { social } = this.props
+    axios.get(`/social/goals/${social}`).then(res => {
+      this.setState({ card: res.data[0] })
+      console.log(res.data)
+    }
+
+    )
+  }
 
 
 
   render() {
     return (
       <div>
-        <h1>Card</h1> 
-        <h1>facebook goals: {this.state.card ? this.state.card : null}</h1>
+        <h1>Card</h1>
+        <h1>facebook likes: {this.state.card ? this.state.card.likes : null}</h1>
       </div>
     )
   }
--- src/ducks/authZero.js
+++ src/ducks/authZero.js
@@ -1,22 +1,26 @@
+// const initialState = {
+//   user: {}
+// }
+
 const initialState = {
-  user: {}
+  user: { id: 5, user_name: "Mason Brockbank", email: "masonbrockbank@gmail.com", auth_id: "google-oauth2|108178934457324039627", picture: "https://lh6.googleusercontent.com/-uPeL5-1RrGE/AAAAAAAAAAI/AAAAAAAAAI8/KLCKeKkMJCw/photo.jpg" }
 }
 
 const UPDATE_USER_DATA = 'UPDATE_USER_DATA'
 
-export function updateUserData(user){
+export function updateUserData(user) {
   return {
-    type:UPDATE_USER_DATA,
+    type: UPDATE_USER_DATA,
     payload: user
   }
 }
 
-export default function reducer (state = initialState, action){
-  switch(action.type){
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
     case UPDATE_USER_DATA:
-      return Object.assign({}, state, {user: action.payload})
+      return Object.assign({}, state, { user: action.payload })
 
-    default: 
+    default:
       return state
   }
 }
